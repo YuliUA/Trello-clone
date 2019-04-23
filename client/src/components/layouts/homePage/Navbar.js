b720c13bb96ca23a8325868adbe059c90b9036e3
Anna <the1anniey@gmail.com>
2019-04-23 23:17:44 +0300

    T-7 added dropdown with logout button

--- client/src/components/layouts/homePage/Navbar.js
+++ client/src/components/layouts/homePage/Navbar.js
@@ -2,21 +2,35 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { logoutUser } from '../../../actions/authActions';
 
 class Navbar extends Component {
+    clickLogoutUser(e) {
+        e.preventDefault();
+        this.props.logoutUser();
+    }
+
     render() {
         const { isAuthenticated, user } = this.props.auth;
         
         const notLoggedInHeader = (
-            <div className="">
+            <div>
                 <Link to="/login" className="btn  btn-link text-white">Увійти</Link>
                 <Link to="/register" className="btn bg-white font-weight-bold text-primary ">Зареєструватися</Link>
             </div>
         )
 
         const loggedInHeader = (
-            <div>
-                <Link to="/" className="btn bg-primary font-weight-bold text-light">{user.firstname}</Link>
+            <div className="loggedInButtons">
+                <Link to="/" className="btn bg-primary font-weight-bold text-light beforeArrow">{user.firstname}</Link>
+
+                <div class="dropdown">
+                    <button class="btn btn-default dropdownArrow" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    </button>
+                    <ul class="dropdown-menu dropdownPosition" aria-labelledby="dropdownMenu1">
+                        <li><a className="text-primary dropdown-item" onClick={this.clickLogoutUser.bind(this)}>Вийти</a></li>
+                    </ul>
+                </div>
             </div>
         )
 
@@ -32,6 +46,7 @@ class Navbar extends Component {
 }
 
 Navbar.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
 };
 
@@ -39,4 +54,4 @@ const mapStateToProps = state => ({
     auth: state.auth,
 });
 
-export default connect(mapStateToProps)(Navbar);
+export default connect(mapStateToProps, {logoutUser})(Navbar);
